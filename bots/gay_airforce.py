fa206d2a92dcfbf0fa3820ead46ca2d5377d2306
JD3Ent <jjd32004@gmail.com>
2025-02-24 21:04:15 -0800

    Update gay_airforce.py

--- bots/gay_airforce.py
+++ bots/gay_airforce.py
@@ -5,7 +5,7 @@ from library.gay_airforce_responses import (
     GAY_AIRFORCE_FINAL_RESPONSES,
 )
 from calculations import calculate_final_sus_points
-from gyatt_team import load_police_records, save_police_records, log_nuked_user
+from gyatt_logic import load_police_records, save_police_records, log_nuked_user
 
 
 async def gay_airforce_interaction(user, message, interaction_data):
